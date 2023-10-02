cedb7c23cb63a45e26a3ae8619081c8113a37211
Jorge S. Sanchez <georotzen@gmail.com>
2023-10-01 22:18:34 -0700

    Initial commit by Georotzen

---
+++ pages/api/jorgess.js
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+    res.status(200).json({ text: 'Jorge Sanchez / Software Engineer - CSULA Spring 2023' });
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,54 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from './components/layout';
 import utilStyles from './styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+import Link from 'next/link';
+import Date from './components/date';
+
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+/** **************************************************************** */
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I am a person just like yourself with dreams of a better world 
-          where everyone can live and enjoy each day doing what is best 
-          for all of us. If there is something valuable in our lives; then, 
+        <p>I am a person just like yourself with dreams of a better world
+          where everyone can live and enjoy each day doing what is best
+          for all of us. If there is something valuable in our lives; then,
           let it be that strong desire to help one another within our communities.</p>
         <p>
           (This is a sample website - you'll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+            {/**  {id} */} {/**  This is the syntax for comments */}
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
